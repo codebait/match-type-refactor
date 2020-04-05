4b7de0f7e0ff000bed5a4dbc02a9d69c3eba3d59
Przemek Zawadzki <przemek.codebait@gmail.com>
2020-04-05 20:13:35 +0200

    change to stream style

--- src/main/java/com/codebait/MatchType.java
+++ src/main/java/com/codebait/MatchType.java
@@ -1,51 +1,44 @@
 package com.codebait;
 
 import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 enum MatchType implements Filter {
   ANY_MATCH {
     @Override
     public boolean filter(String text, Set<String> patterns) {
-      for (String pattern : patterns) {
-        if (Pattern.compile(pattern).matcher(text).find()) {
-          return true;
-        }
-      }
-      return false;
+      return patterns.stream()
+          .map(Pattern::compile)
+          .map(pattern -> pattern.matcher(text))
+          .anyMatch(Matcher::find);
     }
   },
   ALL_MATCH {
     @Override
     public boolean filter(String text, Set<String> patterns) {
-      for (String pattern : patterns) {
-        if (!Pattern.compile(pattern).matcher(text).find()) {
-          return false;
-        }
-      }
-      return true;
+      return patterns.stream()
+          .map(Pattern::compile)
+          .map(pattern -> pattern.matcher(text))
+          .allMatch(Matcher::find);
     }
   },
   ALL_NOT_MATCH {
     @Override
     public boolean filter(String text, Set<String> patterns) {
-      for (String pattern : patterns) {
-        if (Pattern.compile(pattern).matcher(text).find()) {
-          return false;
-        }
-      }
-      return true;
+      return patterns.stream()
+          .map(Pattern::compile)
+          .map(pattern -> pattern.matcher(text))
+          .noneMatch(Matcher::find);
     }
   },
   ANY_NOT_MATCH {
     @Override
     public boolean filter(String text, Set<String> patterns) {
-      for (String pattern : patterns) {
-        if (!Pattern.compile(pattern).matcher(text).find()) {
-          return true;
-        }
-      }
-      return false;
+      return patterns.stream()
+          .map(Pattern::compile)
+          .map(pattern -> pattern.matcher(text))
+          .anyMatch(matcher -> !matcher.find());
     }
   }
 }
