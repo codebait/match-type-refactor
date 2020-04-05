d38a2c0ef53afd3c85f0a7d3c45dc3bec9a44513
Przemek Zawadzki <przemek.codebait@gmail.com>
2020-04-05 20:32:32 +0200

    change to stream style using Predicate

--- src/main/java/com/codebait/MatchType.java
+++ src/main/java/com/codebait/MatchType.java
@@ -1,44 +1,30 @@
 package com.codebait;
 
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 enum MatchType implements Filter {
-  ANY_MATCH {
-    @Override
-    public boolean filter(String text, Set<String> patterns) {
-      return patterns.stream()
-          .map(Pattern::compile)
-          .map(pattern -> pattern.matcher(text))
-          .anyMatch(Matcher::find);
-    }
-  },
-  ALL_MATCH {
-    @Override
-    public boolean filter(String text, Set<String> patterns) {
-      return patterns.stream()
-          .map(Pattern::compile)
-          .map(pattern -> pattern.matcher(text))
-          .allMatch(Matcher::find);
-    }
-  },
-  ALL_NOT_MATCH {
-    @Override
-    public boolean filter(String text, Set<String> patterns) {
-      return patterns.stream()
-          .map(Pattern::compile)
-          .map(pattern -> pattern.matcher(text))
-          .noneMatch(Matcher::find);
-    }
-  },
-  ANY_NOT_MATCH {
-    @Override
-    public boolean filter(String text, Set<String> patterns) {
-      return patterns.stream()
-          .map(Pattern::compile)
-          .map(pattern -> pattern.matcher(text))
-          .anyMatch(matcher -> !matcher.find());
-    }
+  ANY_MATCH(stream -> stream.anyMatch(Matcher::find)),
+  ALL_MATCH(stream -> stream.allMatch(Matcher::find)),
+  ALL_NOT_MATCH(stream -> stream.noneMatch(Matcher::find)),
+  ANY_NOT_MATCH(stream -> stream.anyMatch(matcher -> !matcher.find()));
+  private final Predicate<Stream<Matcher>> matchPredicate;
+
+  MatchType(Predicate<Stream<Matcher>> matchPredicate) {
+    this.matchPredicate = matchPredicate;
+  }
+
+  @Override
+  public boolean filter(String text, Set<String> patterns) {
+    return matchPredicate.test(generateMatcherStream(text, patterns));
+  }
+
+  private Stream<Matcher> generateMatcherStream(String text, Set<String> patterns) {
+    return patterns.stream()
+        .map(Pattern::compile)
+        .map(pattern -> pattern.matcher(text));
   }
 }
